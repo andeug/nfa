0b8d3ae9347948a2d594d535b8bc12ce835dc2c0
andrew <andrewsimiyu1@gmail.com>
2018-08-27 14:49:42 +0300

    1st commit

---
+++ src/nfa/Nfa.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nfa;
+
+
+import java.util.Scanner;
+public class Nfa {
+    
+  public static int state=0;
+  public static int amount=0;
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in); 
+       
+               
+               
+       while(state!=3&&amount!=60&&amount<=60) {
+           
+           System.out.println("current amount:" +amount);
+           System.out.println("Enter amount:" );  
+            
+          int rollno=sc.nextInt();
+          int value=inputRead(rollno);
+          if(value==5){
+              break;
+          }
+          
+          
+           
+       } 
+       
+       if(state==3){
+          System.out.println("Accepting"); 
+       }
+       else{
+          System.out.println("Non-Accepting"); 
+       }
+     
+        
+   
+   
+  
+    }
+    
+    
+    
+    public static int inputRead(int rollno){
+        
+         if (rollno==20){
+             amount=amount+rollno;
+         return state++;
+   }else if(rollno==40){
+       //go to state 2
+       amount=amount+rollno;
+       return state+=2;
+   
+    }else{
+       return 5;
+   }
+        
+         }
+     
+  }
+        
